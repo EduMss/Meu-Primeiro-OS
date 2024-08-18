0e9fe26c3390d0d21eeae403261291613d0c4c79
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:04:54 -0300

    novo codigo boot arm

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -1,18 +1,18 @@
-    .section .text
-    .global _start
+.section .text
+.global _start
 
 _start:
     // Inicializar o sistema (Apenas exemplo, pode variar)
-    mov r0, #1           // Número do file descriptor (1 para stdout)
-    ldr r1, =message     // Endereço da mensagem
-    mov r2, #13          // Tamanho da mensagem
-    mov r7, #4           // Número da syscall para sys_write
-    svc 0                // Chamar o kernel
+    mov r0, #1          // Número do file descriptor (1 para stdout)
+    ldr r1, =message    // Endereço da mensagem
+    mov r2, #13         // Tamanho da mensagem
+    mov r7, #4          // Número da syscall para sys_write
+    svc #0             // Chamar o kernel
 
     // Finalizar o processo
-    mov r7, #1           // Número da syscall para exit
-    mov r0, #0           // Código de saída 0
-    svc 0                // Chamar o kernel
+    mov r7, #1          // Número da syscall para exit
+    mov r0, #0          // Código de saída 0
+    svc #0             // Chamar o kernel
 
 .section .data
 message:
