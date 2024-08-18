1aa0f15d794d1a06ea85c2f085040dae8bc9f742
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:25:41 -0300

    mudando codigo

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -2,15 +2,15 @@
 .global _start
 
 _start:
-    mov r0, #1
-    ldr r1, =message
-    mov r2, #16
-    mov r7, #4
-    svc #0
+    mov r0, #1              @ File descriptor (stdout)
+    ldr r1, =message        @ Load address of the message
+    mov r2, #16             @ Message length
+    mov r7, #4              @ sys_write
+    svc #0                  @ Call kernel
 
-    mov r7, #1
-    mov r0, #0
-    svc #0
+    mov r7, #1              @ sys_exit
+    mov r0, #0              @ Exit status 0
+    svc #0                  @ Call kernel
 
 .section .data
 message:
