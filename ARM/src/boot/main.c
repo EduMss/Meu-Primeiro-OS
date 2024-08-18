054b4a7856ec6022618f71a7584e19e7d1919987
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 17:39:44 -0300

    a

---
+++ ARM/src/boot/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern void _start();
+
+int main() {
+    _start();
+    return 0;
+}
--- ARM/src/boot/soma.s
+++ ARM/src/boot/soma.s
@@ -1,14 +1,18 @@
-.global _start
+.section .rodata
+fmt: .asciz "A soma é: %d\n"
 
+.text
 _start:
-    # ... (código anterior)
+    # Carrega os números a serem somados nos registradores
+    mov r0, #5
+    mov r1, #7
+
+    # Soma os números
+    add r2, r0, r1
 
     # Imprime o resultado
     ldr r0, =fmt
     mov r1, r2
-    bl _printf
+    bl printf
 
-    # ... (código para encerrar)
-
-.section .rodata
-fmt: .asciz "A soma é: %d\n"
+    # Encerra o programa (não é necessário, pois a biblioteca C cuida disso)
