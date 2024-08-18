5fa02f1b9784fc91ae799421c163ebde75059785
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:32:36 -0300

    a

--- ARM/build.sh
+++ ARM/build.sh
@@ -1,5 +1,3 @@
-rm ./src/boot/build/boot.bin \
-&& rm ./src/boot/build/boot.img \
-&& nasm ./src/boot/boot.asm -f bin -o ./src/boot/build/boot.bin \
-&& cp ./src/boot/build/boot.bin ./src/boot/build/boot.img \
-&& truncate -s 1440k ./src/boot/build/boot.img
+&& arm-none-eabi-as -o ./src/boot/boot.o boot.asm \
+&& arm-none-eabi-ld -o ./src/boot/boot boot.o \
+&& arm-none-eabi-objcopy -O binary ./src/boot/boot ./src/boot/boot.bin
