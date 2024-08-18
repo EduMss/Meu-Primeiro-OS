6fc7bacb393e21f97b9dd81c43c60eb0fff4fc52
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:01:43 -0300

    a

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -3,16 +3,16 @@
 
 _start:
     // Inicializar o sistema (Apenas exemplo, pode variar)
-    mov r0, #1           // Número da syscall para write
+    mov r0, #1           // Número do file descriptor (1 para stdout)
     ldr r1, =message     // Endereço da mensagem
     mov r2, #13          // Tamanho da mensagem
-    mov r7, #4           // Número da syscall para sys_write (número 4 no Linux ARM)
-    svc #0               // Chamar o kernel
+    mov r7, #4           // Número da syscall para sys_write
+    svc 0                // Chamar o kernel
 
     // Finalizar o processo
     mov r7, #1           // Número da syscall para exit
     mov r0, #0           // Código de saída 0
-    svc #0               // Chamar o kernel
+    svc 0                // Chamar o kernel
 
 .section .data
 message:
