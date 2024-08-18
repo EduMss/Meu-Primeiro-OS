a1c5fec500a53874a6ce0436597edd319b45993f
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:17:31 -0300

    a

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -2,15 +2,15 @@
 .global _start
 
 _start:
-    mov r0, #1          
-    ldr r1, =message   
-    mov r2, #13         
-    mov r7, #4         
-    svc 0               
+    mov r0, #1
+    ldr r1, message
+    mov r2, #16
+    mov r7, #4
+    svc 0
 
-    mov r7, #1         
-    mov r0, #0          
-    svc 0               
+    mov r7, #1
+    mov r0, #0
+    svc 0
 
 .section .data
 message:
