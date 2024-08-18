e816130452ae5decbb581adf041f5c7c341ed166
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:20:04 -0300

    hexa

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -2,14 +2,14 @@
 .global _start
 
 _start:
-    mov r0, #1
+    mov r0, 0x01
     ldr r1, message
-    mov r2, #16
-    mov r7, #4
+    mov r2, 0x10
+    mov r7, 0x04
     svc 0
 
-    mov r7, #1
-    mov r0, #0
+    mov r7, 0x01
+    mov r0, 0x00
     svc 0
 
 .section .data
