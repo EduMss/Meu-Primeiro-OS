0368ad5d35aca88ff4dc30e796b1601febf3aaa6
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:33:47 -0300

    a

--- ARM/build.sh
+++ ARM/build.sh
@@ -1,3 +1,3 @@
-&& arm-none-eabi-as -o ./src/boot/boot.o boot.asm \
+arm-none-eabi-as -o ./src/boot/boot.o boot.asm \
 && arm-none-eabi-ld -o ./src/boot/boot boot.o \
 && arm-none-eabi-objcopy -O binary ./src/boot/boot ./src/boot/boot.bin
