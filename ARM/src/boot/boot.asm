1af2f782b0b2022c825de0061b3812ad62d2c491
EduMss <eduardo555gatinho@hotmail.com>
2024-08-18 10:12:28 -0300

    mudando codigo

--- ARM/src/boot/boot.asm
+++ ARM/src/boot/boot.asm
@@ -4,7 +4,7 @@
 _start:
     ; Inicializar o sistema (Apenas exemplo, pode variar)
     mov r0, 1          ; Número do file descriptor (1 para stdout)
-    ldr r1, =message    ; Endereço da mensagem
+    ldr r1, message    ; Endereço da mensagem
     mov r2, 13         ; Tamanho da mensagem
     mov r7, 4          ; Número da syscall para sys_write
     svc 0             ; Chamar o kernel
