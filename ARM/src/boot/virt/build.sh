0d8f5db50602437f63b155842c12ed6938efba43
EduMss <127635259+EduMss@users.noreply.github.com>
2024-08-19 08:58:50 -0400

    a

--- ARM/src/boot/virt/build.sh
+++ ARM/src/boot/virt/build.sh
@@ -1,3 +1,3 @@
 arm-none-eabi-as -o boot.o boot.s \
-&& arm-none-eabi-ld -Ttext=0x10000 -o boot.elf boot.o \
+&& arm-none-eabi-ld -Ttext=0x40000000 -o boot.elf boot.o \
 && arm-none-eabi-objcopy -O binary boot.elf boot.bin
