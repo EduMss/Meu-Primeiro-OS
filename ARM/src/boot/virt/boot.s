d438d0827a83eaff562b683faee31ffd68af9ae0
EduMss <127635259+EduMss@users.noreply.github.com>
2024-08-19 08:53:39 -0400

    a

--- ARM/src/boot/virt/boot.s
+++ ARM/src/boot/virt/boot.s
@@ -1,32 +1,29 @@
+.section .text
 .global _start
 
-.equ UART0_BASE, 0x09000000  @ Tente também com 0x09000000 se não funcionar
-.equ UARTFR, 0x18
-.equ UARTFR_TXFF, 0x20
-.equ UARTDR, 0x00
+UART0_BASE = 0x09000000
+UART0_DR = UART0_BASE + 0x00
 
-.section .text
 _start:
-    ldr r0, =message          @ Carrega o endereço da mensagem
-    bl print_string           @ Chama a função para imprimir a string
+    ldr r0, =message
+    bl print_string
     b .
 
 print_string:
-    ldrb r1, [r0], #1         @ Carrega o próximo byte da string
-    cmp r1, #0                @ Verifica se é o final da string
-    beq end_print             @ Se for o final, sai da função
+    mov r1, r0
+    loop:
+        ldrb r2, [r1], #1
+        cmp r2, #0
+        beq end
+        bl putchar
+        b loop
+    end:
+        bx lr
 
-wait_uart:
-    ldr r2, =UART0_BASE       @ Carrega o endereço base do UART0
-    ldr r3, [r2, #UARTFR]     @ Carrega o status do UART0
-    tst r3, #UARTFR_TXFF      @ Verifica se o buffer está cheio
-    bne wait_uart             @ Espera se o buffer estiver cheio
-
-    strb r1, [r2, #UARTDR]    @ Escreve o byte no UART0
-    b print_string            @ Imprime o próximo caractere
-
-end_print:
-    bx lr                     @ Retorna
+putchar:
+    ldr r1, =UART0_DR
+    strb r2, [r1]
+    bx lr
 
 .section .data
 message:
