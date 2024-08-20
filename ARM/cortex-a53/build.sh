3de20e8bde3273c53bf21038945ef791de90294b
EduMss <127635259+EduMss@users.noreply.github.com>
2024-08-19 20:39:16 -0400

    a

--- ARM/cortex-a53/build.sh
+++ ARM/cortex-a53/build.sh
@@ -1,3 +1,3 @@
 aarch64-linux-gnu-as -o boot.o boot.s \
-&& aarch64-linux-gnu-ld -Ttext=0x10000 -o boot.elf boot.o \
+&& aarch64-linux-gnu-ld -o boot.elf boot.o \
 && aarch64-linux-gnu-objcopy -O binary boot.elf boot.bin
