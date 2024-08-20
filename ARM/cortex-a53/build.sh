6fcdd3690ec148f3e6501750a8ce2b081a31bbad
EduMss <127635259+EduMss@users.noreply.github.com>
2024-08-19 20:09:51 -0400

    a

--- ARM/cortex-a53/build.sh
+++ ARM/cortex-a53/build.sh
@@ -1,3 +1,3 @@
-arm-none-eabi-as -o boot.o boot.s \
+arm-none-eabi-as -march=armv8-a -o boot.o boot.s \
 && arm-none-eabi-ld -Ttext=0x10000 -o boot.elf boot.o \
 && arm-none-eabi-objcopy -O binary boot.elf boot.bin
