79f1e8b8221fe6b3f65b9962ba5242a1fbe04462
EduMss <eduardo555gatinho@hotmail.com>
2024-11-21 17:20:34 -0300

    Mensagem inicial

--- x86/src/Objetivos/1/projeto.asm
+++ x86/src/Objetivos/1/projeto.asm
@@ -1,7 +1,7 @@
 section .data
     hello_msg db "Olá ", 0       ; Prefixo da mensagem
     welcome_msg db ", Seja Bem-vindo!", 0xA, 0 ; Sufixo da mensagem com nova linha
-    Fist_msg db "Informe seu nome: ", 0xA    ; Mensagem peguntando o nome
+    Fist_msg db "Informe seu nome: ", 0;0xA    ; Mensagem peguntando o nome
     len equ $ - Fist_msg              ; Comprimento da mensagem
 
 section .bss
@@ -14,7 +14,7 @@ global _start
 _start:
     mov eax, 4
     mov ebx, 1
-    mov ecx, Fist_msg                 ; ponteiro para a mensagem
+    mov ecx, Fist_msg            ; ponteiro para a mensagem
     mov edx, len                 ; comprimento da mensagem
     int 0x80                     ; chamada ao kernel
 
