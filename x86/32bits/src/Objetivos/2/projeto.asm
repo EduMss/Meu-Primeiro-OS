6c21e97faa7841995ba2d8283ef4644fb89646f3
EduMss <eduardo555gatinho@hotmail.com>
2024-11-21 20:39:39 -0300

    2

--- x86/32bits/src/Objetivos/2/projeto.asm
+++ x86/32bits/src/Objetivos/2/projeto.asm
@@ -1,19 +1,39 @@
 section .data
     file_buffer dd 0
+    
     nova_msg db "Novo Mundo!", 0xA
     nova_msg_len equ $ - nova_msg
+    
     criado_msg db "ArquivoCriado!", 0xA
     criado_msg_len equ $ - criado_msg
-    filename db "example.txt", 0 
+
+    ;filename db "example.txt", 0 
+
+    inicial_msg db "Nome do arquivo desejado: ", 0
+    inicial_msg_len equ $ - inicial_msg
+
 
 section .bss
     buffer resb 100                ; Buffer para armazenar a leitura (100 bytes)
-    ;filename resb 100
+    filename resb 100
     
 section .text
     global _start
 
 _start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, inicial_msg            ; ponteiro para a mensagem
+    mov edx, inicial_msg_len                 ; comprimento da mensagem
+    int 0x80                     ; chamada ao kernel
+
+    ; Usuario escrevendo nome
+    mov eax, 3  ; ler terminal
+    mov ebx, 0  ; sdtin
+    mov ecx, filename   ; endereço de memória para armazenar a entrada
+    mov edx, 100          ; número máximo de bytes a ler (tamanho do buffer)
+    int 0x80             ; chamada ao kernel
+
     ; Abrir o arquivo para leitura
     mov eax, 5          ; syscall: sys_open
     mov ebx, filename   ; nome do arquivo
@@ -23,7 +43,7 @@ _start:
 
     ; Verificar se houve erro na abertura do arquivo (se o arquivo não foi aberto corretamente)
     test eax, eax                 ; Se eax for negativo, ocorreu um erro
-    js .criar_arquivo                      ; Se erro, sair
+    js .criar_arquivo                      ; Se erro, ele vai criar o arquivo, depois vai dar contninuidade no codigo
 
     jmp .continuidade
 
@@ -54,6 +74,8 @@ _start:
     mov eax, 1                    ; syscall número 1 para exit
     xor ebx, ebx                  ; código de saída (0)
     int 0x80                      ; chamada ao kernel
+
+
 
 .exit:
     ; Finalizar o programa
