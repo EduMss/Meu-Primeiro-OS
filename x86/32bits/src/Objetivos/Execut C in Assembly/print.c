2fd22cfe971685e9bb4a0baac76b052889029d83
EduMss <eduardo555gatinho@hotmail.com>
2024-11-24 10:18:41 -0300

    Executar C in Assembly

--- x86/32bits/src/Objetivos/Execut C in Assembly/print.c
+++ x86/32bits/src/Objetivos/Execut C in Assembly/print.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 // gcc -m32 -c print.c -o print
 int main() {
-    char phone[20] = "";
-    fgets(phone, 19, stdin);
-    fprintf(stdout, "Hello stdout\n");
-    fprintf(stderr, "Hello stderr\n");
+    printf("hello world\n");
 }
